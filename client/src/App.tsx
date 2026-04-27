3c2884cc7f6ad90f56c78b046fd187032fc13065
Bhaumik Joshi <imbhaumikjoshiforyou@gmail.com>
2026-04-28 05:23:10 +0530

    added ThemeProvider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { ThemeProvider, CssBaseline } from "@mui/material"
+import { AuthProvider } from "./context/AuthContext"
+import { LandingPage } from "./components/home"
+import theme from "./styles/home/theme"
+
 function App() {
   return (
-    <h1>Merge Safe</h1>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AuthProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+          </Routes>
+        </Router>
+      </AuthProvider>
+    </ThemeProvider>
   )
 }
 
