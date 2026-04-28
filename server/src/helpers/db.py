f1e422b19f6d2f052e7f4520d598ef50f1fddddb
Bhaumik Joshi <imbhaumikjoshiforyou@gmail.com>
2026-04-28 20:31:13 +0530

    added ssl support for prod

--- server/src/helpers/db.py
+++ server/src/helpers/db.py
@@ -1,4 +1,5 @@
 import os
+import ssl
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from sqlalchemy.orm import declarative_base
 from dotenv import load_dotenv
@@ -6,11 +7,20 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+IS_PROD = os.getenv("PROD", "false") == "true"
 
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL environment variable is not set")
 
-engine = create_async_engine(DATABASE_URL, echo=False)
+connect_args = {}
+if IS_PROD:
+    connect_args["ssl"] = ssl.create_default_context()
+
+engine = create_async_engine(
+    DATABASE_URL,
+    echo=False,
+    connect_args=connect_args
+)
 AsyncSessionLocal = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 Base = declarative_base()
 
