c8620edcd09a3eb5fe7aa99a7233a74db46daeeb
Bhaumik Joshi <imbhaumikjoshiforyou@gmail.com>
2026-04-28 05:34:17 +0530

    optimized bundle size

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,5 +6,19 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes('node_modules')) {
+            if (id.includes('@mui')) {
+              return 'vendor_mui';
+            }
+            return 'vendor';
+          }
+        }
+      }
+    }
   }
 })
